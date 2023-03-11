241e46645ec5ff9f4c0d42ce2148b6398da2c4e3
JohnDSmallpage <john.smallpage@correo.unimet.edu.ve>
2023-03-10 23:11:58 -0400

    Cargadas librerias

---
+++ Microproyecto 2/src/firebase/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDI0LcEa7bicKOllOiZT6mGgOV9OxcaQGw",
+  authDomain: "microproyecto-2-ac799.firebaseapp.com",
+  projectId: "microproyecto-2-ac799",
+  storageBucket: "microproyecto-2-ac799.appspot.com",
+  messagingSenderId: "279790915019",
+  appId: "1:279790915019:web:5fdc4aee5bf1544337ba4e",
+  measurementId: "G-KEWCNL619N"
+};
+
+const app = initializeApp(firebaseConfig);
