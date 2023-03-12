bde70c7d75481d6b82c86993ed62f65ba0e089c2
GabrielSuarez <suarezgabrielr@gmail.com>
2023-03-12 18:21:14 -0400

    landing page

--- Microproyecto 2/src/Pages/LandingPage/LandingPage.jsx
+++ Microproyecto 2/src/Pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import background from '../../imagenes/fondo2.png'
+import { Link } from 'react-router-dom'
+import { LOGIN_URL, REGISTER_URL } from '../../Urls/Urls'
 
 export function LandingPage() {
   return (
-    <div>LandingPage</div>
+    <div id='container' style={{ backgroundImage: `url(${background})` }} className="flex items-center justify-center bg-black bg-opacity-50 bg-contain bg-cover w-full">
+      
+      <div className='flex items-center flex-col justify-center h-[500px] justify-evenly'>
+        <p className='text-white text-2xl font-extrabold text-center'>Busca las mejores peliculas y cuando Verlas</p>
+      <div className='flex flex-row justify-evenly w-full'>
+        <Link className='text-white bg-indigo-800 p-2 rounded-lg hover:bg-indigo-600 border-2 border-black' to={LOGIN_URL}>Inicia Sesion</Link>
+        <Link className='text-white bg-indigo-800 p-2 rounded-lg hover:bg-indigo-600 border-2 border-black' to={REGISTER_URL}>Registrate</Link>
+      </div>
+      </div>
+    </div>
   )
 }
