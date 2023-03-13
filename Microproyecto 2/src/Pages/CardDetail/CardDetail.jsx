e1207f5f9b3d4c645d12fd9f845c9903e50bfd51
SebasAure <saure@correo.unimet.edu.ve>
2023-03-12 20:48:22 -0400

    responsive carddetail

--- Microproyecto 2/src/Pages/CardDetail/CardDetail.jsx
+++ Microproyecto 2/src/Pages/CardDetail/CardDetail.jsx
@@ -1,96 +1,99 @@
-import React from 'react'
-import { useState } from 'react';
-import { useParams } from 'react-router';
-import { useEffect } from 'react';
-import { getSingleMovie } from '../../utils/moviesApi';
-import estrellita from '../../imagenes/estrellita.png';
+import React from "react";
+import { useState } from "react";
+import { useParams } from "react-router";
+import { useEffect } from "react";
+import { getSingleMovie } from "../../utils/moviesApi";
+import estrellita from "../../imagenes/estrellita.png";
 
 export function CardDetail() {
-    const [movie, setMovie] = useState([]);
-    let variable;
-    const {movie_id} = useParams();
-    const getMovie= async (movie_id) => {
-        const {data} = await getSingleMovie(movie_id);
-        console.log(data);
-        setMovie(data);
-      }
+  const [movie, setMovie] = useState([]);
+  let variable;
+  const { movie_id } = useParams();
+  const getMovie = async (movie_id) => {
+    const { data } = await getSingleMovie(movie_id);
+    console.log(data);
+    setMovie(data);
+  };
 
-      useEffect( () => {
-        getMovie(movie_id);
-      }, [])
+  useEffect(() => {
+    getMovie(movie_id);
+  }, []);
 
   return (
-    <div id= 'main-container' className='flex flex-col text-white p-6'>
-        <div id='title' className='mb-2 h-1/5' >
-            <h1>{movie.title}</h1>
-            <p>{movie.release_date}</p>
+    <div id="main-container" className="flex flex-col text-white px-[15px]">
+      <div id="title" className="mb-2 h-1/5 
+      sm:text-4xl
+      md:text-5xl
+      lg:text-6xl
+      xl:text-7xl">
+        <h1>{movie.title}</h1>
+        <p>{movie.release_date}</p>
+      </div>
+
+      <div
+        id="middle-container"
+        className="flex flex-row justify-between h-2/5 "
+      >
+        <div id="poster" className="w-1/2">
+          <img
+            className="w-screen"
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt=""
+          />
         </div>
 
-        <div id='middle-container' className='flex flex-row justify-between h-2/5'>
-            <div id='poster' className='w-1/2'>
-                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="" />
-            </div>
+        <div
+          id="info"
+          className="flex flex-col ml-3 w-1/2  text-justify 
+          text-xs
+          sm:text-2xl
+          md:text-3xl
+          lg:text-4xl
+          "
+        >
+          <div className="h-1/5 flex justify-center items-center overflow-y-scroll mb-[-10px]">
+            {movie.genres?.map((genre, idx) => (variable = genre.name + " "))}
+          </div>
+          <hr className="my-2" />
+          <p className="h-3/5 overflow-y-scroll">{movie.overview}</p>
 
-            <div id='info' className='flex flex-col ml-3 w-1/2 text-xs text-justify'>
-                <div className='h-1/5 flex justify-center items-center overflow-y-scroll mb-[-10px]'>
-                {
-                    
-                    movie.genres?.map( (genre, idx) => (
-                        variable= genre.name + " "      
-                    ) )
-                   
-                }
-                
-                </div>
-                <hr className='my-2'/>
-                <p className='h-3/5 overflow-y-scroll'>{movie.overview}</p>
-                
-                <hr className='my-2'/>
-                <div className='h-1/5 flex flex-col justify-center items-center'>
-                    
-                    <img src={estrellita} alt="" className='w-1/5 h-1/2' />
-                    <p className='h-1/2 mt-2'>{movie.vote_average} /10</p>
-                    
-                </div>
-               
-                
-            </div>
+          <hr className="my-2" />
+          <div className="h-1/5 flex flex-col justify-center items-center">
+            <img src={estrellita} alt="" className="w-1/5 h-1/2" />
+            <p className="h-1/2 mt-2">{movie.vote_average} /10</p>
+          </div>
         </div>
-        <hr className='my-2'/>
-        <div id='bottom-container' className='h-2/5'>
-                <p>
-                    Productora: 
-                {
-                    
-                    movie.production_companies?.map( (company, idx) => (
-                        variable= " " + company.name 
-                    ) )
-                   
-                }
-                </p>
-                <hr className='my-2'/>
-                <p>
-
-                Idiomas: 
-                {
-                    
-                    movie.spoken_languages?.map( (language, idx) => (
-                        variable= " " + language.name 
-                    ) )
-                   
-                }
-                    
-                </p>
-                <hr className='my-2'/>
-                <p>
-                    Presupuesto: {movie.budget}$
-                </p>
-                <hr className='my-2'/>
-                <p>
-                    Estado: {movie.status}
-                </p>
-                <hr className='my-2'/>
-        </div>
+      </div>
+      <hr className="my-2" />
+      <div id="bottom-container" className="h-2/5 
+      sm:text-2xl
+      md:text-3xl
+      lg:text-4xl"
+      >
+        <p>
+          Productora:
+          {movie.production_companies?.map(
+            (company, idx) => (variable = " " + company.name)
+          )}
+        </p>
+        <hr className="my-2" />
+        <p>
+          Idiomas:
+          {movie.spoken_languages?.map(
+            (language, idx) => (variable = " " + language.name)
+          )}
+        </p>
+        <hr className="my-2" />
+        <p>Presupuesto: {movie.budget}$</p>
+        <hr className="my-2" />
+        <p>Estado: {movie.status}</p>
+        <hr className="my-2" />
+      </div>
     </div>
-  )
+
+
+
+
+
+  );
 }
